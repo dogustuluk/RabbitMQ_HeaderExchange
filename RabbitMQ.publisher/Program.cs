02eb671c24f475fc1363e951fe8645e3fa217997
dogustuluk <dogus.tuluk@gmail.com>
2022-05-05 16:19:29 +0300

    publisher created

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace RabbitMQ.publisher
 {
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://nxdranwu:n_3nr-xZlXx0NoCWuFP05gTqZfp7_hwK@sparrow.rmq.cloudamqp.com/nxdranwu");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-headers", durable:true, type: ExchangeType.Headers);
+
+            Random rnd = new Random();
+
+            Enumerable.Range(1, 100).ToList().ForEach(x =>
+            {
+                LogNames log1 = (LogNames)rnd.Next(1, 5);
+                LogNames log2 = (LogNames)rnd.Next(1, 5);
+                LogNames log3 = (LogNames)rnd.Next(1, 5);
+                var routeKey = $"{log1}.{log2}.{log3}";
+
+                var message = $"log-type: {log1}-{log2}-{log3}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-headers", routeKey, null, messageBody);
+                Console.WriteLine($"log gönderilmiştir {message}");
+            });
         }
     }
 }
